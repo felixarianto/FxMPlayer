77e0a04e786a1589295565c5209e5d75231c1adf
febri <felixarianto13@gmail.com>
2018-09-16 08:43:14 +0700

    ...

--- app/src/main/java/com/fx/app/fxmplayer/MainActivity.java
+++ app/src/main/java/com/fx/app/fxmplayer/MainActivity.java
@@ -14,6 +14,8 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
@@ -24,6 +26,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.fx.app.sqlite.DB;
@@ -44,6 +47,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rcvw_track = findViewById(R.id.rcvw_track);
         rcvw_track.setAdapter(mAdapterTrack = new Adapter());
 
+        DividerItemDecoration divider = new DividerItemDecoration(this, LinearLayoutManager.VERTICAL);
+        divider.setDrawable(getResources().getDrawable(R.drawable.divider_list));
+        rcvw_track.addItemDecoration(divider);
+
         btn_add = findViewById(R.id.btn_add);
         btn_add.setOnClickListener(this);
         txt_title = findViewById(R.id.txt_title);
@@ -54,6 +61,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         seek_line = findViewById(R.id.seek_line);
         seek_start = findViewById(R.id.seek_start);
         seek_end   = findViewById(R.id.seek_end);
+
+        seek_pos_sc = findViewById(R.id.seek_pos_sc);
+        seek_pos_ec = findViewById(R.id.seek_pos_ec);
 
         mOnNavigationItemSelectedListener
         = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -140,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     cr.close();
 
                     mAdapterTrack.DATA.clear();
-                    cr = DB.query("select _id, title, sc_time, ec_time from FILE_TRACK order by sq_no desc limit 1");
+                    cr = DB.query("select _id, title, sc_time, ec_time from FILE_TRACK order by sq_no desc");
                     while (cr.moveToNext()) {
                         mAdapterTrack.DATA.add(new Object[]{cr.getInt(0), cr.getString(1), cr.getInt(2), cr.getInt(3)});
                     }
@@ -157,34 +167,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }.start();
     }
 
-    private void refreshTracks() {
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-
-                    mAdapterTrack.DATA.clear();
-                    Cursor cr = DB.query("select _id, title, sc_time, ec_time from FILE_TRACK order by sq_no desc limit 1");
-                    while (cr.moveToNext()) {
-                        }
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-    }
-
 
     private Adapter mAdapterTrack;
-
-
     private MediaMetadataRetriever mInfo;
     private MediaPlayer mPlayer;
     private Button btn_add;
     private TextView txt_title;
     private ImageButton btn_play_pause;
     private RecyclerView rcvw_track;
+    private int mSelectedTrack = -1;
 
     private void prepare(File file) throws Exception {
         if (mPlayer != null && mPlayer.isPlaying()) {
@@ -201,13 +192,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mPlayer.prepare();
     }
 
-
+    private View seek_pos_sc;
+    private View seek_pos_ec;
     private TextView seek_start;
     private TextView seek_end;
     private View seek_total;
     private View seek_line;
     private Runnable mSeekRunnable;
-    private int[] mSeekSelection = new int[]{0, 0};
+
+    private int min_seek = 0;
+    private int max_seek = 0;
+    private int len_seek = 0;
     private void initializeSeek() {
         if (mSeekRunnable != null) {
             return;
@@ -220,14 +215,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                  */
                 try {
                     ViewGroup.LayoutParams params = seek_line.getLayoutParams();
-                    params.width = seek_total.getWidth() * mPlayer.getCurrentPosition() / mPlayer.getDuration();
+                    params.width = len_seek * (mPlayer.getCurrentPosition() - min_seek) / (max_seek - min_seek);
                     seek_line.setLayoutParams(params);
 
                     seek_start.setText(Util.toDisplay(mPlayer.getCurrentPosition()));
-                    seek_end  .setText(Util.toDisplay(mPlayer.getDuration()));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+
+                if (max_seek <= mPlayer.getCurrentPosition()) {
+                    mPlayer.seekTo(min_seek);//REPEAT
+                }
+
                 if (mSeekRunnable != null) {
                     Handler handler = seek_total.getHandler();
                     if (handler != null) {
@@ -242,8 +241,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void seekSelect(int sc, int ec) {
+        min_seek = sc;
+        max_seek = ec;
+
+        seek_start.setText(Util.toDisplay(sc));
+        seek_end  .setText(Util.toDisplay(ec));
+
+        ViewGroup.LayoutParams params = seek_line.getLayoutParams();
+        params.width = 0;
+        seek_line.setLayoutParams(params);
+
+        int p_sc, p_ec;
+
+        RelativeLayout.LayoutParams
+        sc_params = (RelativeLayout.LayoutParams) seek_pos_sc.getLayoutParams();
+        sc_params.setMargins(p_sc = seek_total.getWidth() * sc / mPlayer.getDuration(), 0, 0, 0);
+        seek_pos_sc.setLayoutParams(sc_params);
+
+        RelativeLayout.LayoutParams
+        ec_params = (RelativeLayout.LayoutParams) seek_pos_ec.getLayoutParams();
+        ec_params.setMargins(p_ec = seek_total.getWidth() * ec / mPlayer.getDuration(), 0, 0, 0);
+        seek_pos_ec.setLayoutParams(ec_params);
+
+        len_seek = p_ec - p_sc;
+
+        mPlayer.seekTo(sc);
+    }
+
 
     private void addTrack() {
+        final Dialog dialog = new Dialog(MainActivity.this);
         View view = View.inflate(MainActivity.this, R.layout.track_add_dialog, null);
         final EditText edtx_title = view.findViewById(R.id.edtx_title);
         final EditText edtx_start = view.findViewById(R.id.edtx_start);
@@ -258,8 +286,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     if (isChecked) {
                         edtx_start.setEnabled(false);
                         edtx_end  .setEnabled(false);
-                        edtx_start.setText(Util.toDisplay(mSeekSelection[0]));
-                        edtx_end  .setText(Util.toDisplay(mSeekSelection[1]));
+                        edtx_start.setText(Util.toDisplay(min_seek));
+                        edtx_end  .setText(Util.toDisplay(max_seek));
                     }
                     else {
                         edtx_start.setEnabled(true);
@@ -278,13 +306,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void onClick(View view) {
                 try {
                     addTrack(edtx_title.getText().toString(), edtx_start.getText().toString(), edtx_end.getText().toString());
+                    dialog.dismiss();
                 } catch (Exception e) {
                     Log.e("", "", e);
                 }
             }
         });
-
-        Dialog dialog = new Dialog(MainActivity.this);
         dialog.setContentView(view);
         dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         dialog.show();
@@ -294,41 +321,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         new AsyncTask<String, String, Object[]>() {
             @Override
             protected Object[] doInBackground(String... strings) {
+                try {
+                    int sq_no = 1;
+                    Cursor cursor = DB.query("select sq_no from FILE_TRACK where file_id=" + mFileId + " order by sq_no desc limit 1");
+                    if (cursor.moveToFirst()) {
+                        sq_no = cursor.getInt(0) + 1;
+                    }
 
-                int sq_no = 1;
-                Cursor cursor = DB.query("select sq_no from FILE_TRACK where file_id=" + mFileId + " order by sq_no desc limit 1");
-                if (cursor.moveToFirst()) {
-                    sq_no = cursor.getInt(0) + 1;
+                    int sc, ec;
+
+                    ContentValues cvalues = new ContentValues();
+                    cvalues.put("file_id", mFileId);
+                    cvalues.put("title", p_title);
+                    cvalues.put("sq_no", sq_no);
+                    cvalues.put("sc_time", sc = Util.fromDisplay(p_start));
+                    cvalues.put("ec_time", ec = Util.fromDisplay(p_end));
+                    long id = DB.insert("FILE_TRACK", cvalues);
+
+                    return new Object[]{Long.valueOf(id).intValue(), p_title, sc, ec};
+                } catch (Exception e) {
+                    Log.e("", "", e);
                 }
-
-                int sc=0, ec=0;
-
-                ContentValues cvalues = new ContentValues();
-                cvalues.put("file_id", mFileId);
-                cvalues.put("title", p_title);
-                cvalues.put("sq_no", sq_no);
-                cvalues.put("sc_time", sc = Util.fromDisplay(p_start));
-                cvalues.put("ec_time", ec = Util.fromDisplay(p_end));
-                long id = DB.insert("FILE_TRACK", cvalues);
-
-                return new Object[]{Long.valueOf(id).intValue(), p_title, sc, ec};
+                return null;
             }
 
             @Override
             protected void onPostExecute(Object[] r) {
-                mAdapterTrack.DATA.add(r);
-                mAdapterTrack.notifyItemInserted(mAdapterTrack.DATA.size() - 1);
+                try {
+                    mAdapterTrack.DATA.add(r);
+                    mAdapterTrack.notifyItemInserted(mAdapterTrack.DATA.size() - 1);
+                } catch (Exception e) {
+                    Log.e("", "", e);
+                }
             }
         }.execute();
-
-
-//        db.execSQL("CREATE TABLE FILE_TRACK(" +
-//                "_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-//                "title TEXT NOT NULL," +
-//                "sq_no INTEGER NOT NULL," +
-//                "sc_time TEXT NOT NULL," +
-//                "ec_time TEXT NOT NULL" +
-//                ")");
     }
 
     private void addFile() throws Exception {
@@ -369,6 +395,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onPrepared(MediaPlayer mediaPlayer) {
+        seekSelect(0, mPlayer.getDuration());
         txt_title.setText(mInfo.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE) + " " + mInfo.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST));
     }
 
@@ -393,22 +420,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
             View view = viewGroup.inflate(viewGroup.getContext(), R.layout.track_adapter, null);
             Holder holder = new Holder(view);
-            holder.title  = view.findViewById(R.id.title);
-            holder.time   = view.findViewById(R.id.time);
+            try {
+                holder.title  = view.findViewById(R.id.title);
+                holder.time   = view.findViewById(R.id.time);
+            } catch (Exception e) {
+                Log.e("", "", e);
+            }
             return holder;
         }
 
         @Override
         public void onBindViewHolder(@NonNull Holder holder, int index) {
-            Object[] data = DATA.get(index);
-            holder.title.setText((String) data[1]);
-            holder.time .setText(Util.toDisplay((int) data[2]) + " - " + Util.toDisplay((int) data[3]) );
+            try {
+                final Object[] data = DATA.get(index);
+
+                boolean is_selected = (int) data[0] == mSelectedTrack;
+
+                holder.title.setEnabled(is_selected);
+                holder.time .setEnabled(is_selected);
+
+                holder.itemView.setBackgroundColor(is_selected ? getResources().getColor(R.color.colorPrimaryDarkLight) : getResources().getColor(android.R.color.transparent));
+
+                holder.title.setText((String) data[1]);
+                holder.time .setText(Util.toDisplay((int) data[2]) + " - " + Util.toDisplay((int) data[3]) );
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        try {
+                            if (mSelectedTrack == (int) data[0]) {
+                                return;
+                            }
+
+                            if (mPlayer.isPlaying()) {
+                                btn_play_pause.callOnClick();
+                            }
+
+                            int last_selected = mSelectedTrack;
+                            mSelectedTrack    = (int) data[0];
+
+                            mAdapterTrack.notifyItemChanged(indexOf(mSelectedTrack));
+                            if (last_selected != -1) mAdapterTrack.notifyItemChanged(indexOf(last_selected));
+
+                            seekSelect((int) data[2], (int) data[3]);
+                        } catch (Exception e) {
+                            Log.e("", "", e);
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                Log.e("", "", e);
+            }
         }
 
         @Override
         public int getItemCount() {
             return DATA.size();
         }
+
+        int indexOf(int pId) {
+            for (int i = 0; i < DATA.size(); i++) {
+                if (pId == (int) DATA.get(i)[0]) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
     }
 
 
--- app/src/main/java/com/fx/app/fxmplayer/Util.java
+++ app/src/main/java/com/fx/app/fxmplayer/Util.java
@@ -28,10 +28,18 @@ public class Util {
     }
 
     public static int fromDisplay(String duration) {
+        int hour=0, minute=0, second=0;
         String[] time = duration.split(":");
-        int hour   = Integer.valueOf(time[0]) * 3600000;
-        int minute = Integer.valueOf(time[1]) * 60000;
-        return hour + minute;
+        if (time.length == 3) {
+            hour   = Integer.valueOf(time[0]) * 3600000;
+            minute = Integer.valueOf(time[1]) * 60000;
+            second = Integer.valueOf(time[2]) * 1000;
+        }
+        else if (time.length == 2) {
+            minute = Integer.valueOf(time[0]) * 60000;
+            second = Integer.valueOf(time[1]) * 1000;
+        }
+        return hour + minute + second;
     }
 
     private static String twoDigit(long v) {
